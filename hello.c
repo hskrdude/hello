9658df3252db672799fe49c8b0b1ea5c69a4df66
Evan Howell <ehowell6@huskers.unl.edu>
2023-08-24 12:17:28 -0500

    added my major to output

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**Author: Evan Howell
+Program: Hack01 (C)
+Purpose: to complete the c version of hack 1?
+Date: 8/24/2023
+*/
+
+
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main (int argc, char **argv) {
+    printf ("Evan Howell!\n");
+    printf ("Computer Science\n");
+    return 0;
+}
